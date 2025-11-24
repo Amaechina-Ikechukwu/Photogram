d59cc2f16563af327d4dad149319d533305358bc
Ikechukwu-Okwuchukwu-Amaechina <vbe05620bikechukwu@gmail.com>
2025-11-24 17:22:53 +0100

    Debugging

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,31 +11,11 @@ import { ActivityIndicator } from 'react-native';
 import { ToastProvider } from '@/components/ToastProvider';
 import { AuthProvider } from '../context/AuthContext';
 import { UploadProvider } from '../context/UploadContext';
-import * as Sentry from '@sentry/react-native';
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
-Sentry.init({
-  dsn: 'https://8eb63f46e6e841b788e1da7803b99b09@o4507437373980672.ingest.de.sentry.io/4510407908982864',
-
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
-
-  // Enable Logs
-  enableLogs: true,
-
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
-});
-
-export default Sentry.wrap(function RootLayout() {
+export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -77,4 +57,4 @@ export default Sentry.wrap(function RootLayout() {
       </UploadProvider>
     </AuthProvider>
   );
-});
+}
--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,6 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
-
-const defaultConfig = getSentryExpoConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
 // Add TypeScript extensions
 defaultConfig.resolver.sourceExts.push('ts', 'tsx', 'cjs');
